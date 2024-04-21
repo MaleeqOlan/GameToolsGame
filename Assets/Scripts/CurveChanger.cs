101606f718bc213c4be4b73f9c636027728b76ec
LuckyIRL <C23775565@mytudublin.ie>
2024-04-21 02:15:48 +0100

    RunForTheBorder

---
+++ Assets/Scripts/CurveChanger.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurveChanger : MonoBehaviour
+{
+    public Material[] myMaterials;
+
+    private float currentValue;
+    private float targetValue;
+
+    public float lerpTime;
+
+    private bool isComplete = true;
+
+    private void Start()
+    {
+        foreach (Material material in myMaterials)
+        {
+            currentValue = material.GetFloat("_SidewaysStrength");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag ("Curve"))
+        {
+            if (isComplete) 
+            {
+                StartCoroutine(changeCurveStrength());
+            }
+            
+        }
+    }
+
+    public IEnumerator changeCurveStrength() 
+    {
+        float elapsedTime = 0;
+        targetValue = Random.Range(-0.005f, 0.005f);
+
+        while (elapsedTime < lerpTime)
+        {
+            isComplete = false;
+            
+            currentValue = Mathf.Lerp(currentValue, targetValue, (elapsedTime / lerpTime));
+            elapsedTime += Time.deltaTime;
+
+
+            foreach (Material material in myMaterials)
+            {
+                material.SetFloat("_SidewaysStrength", currentValue);
+            }
+
+            yield return null;
+        }
+
+        isComplete = true;
+        currentValue = targetValue;
+    }
+}
---
+++ Assets/Scripts/SectionTrigger.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+public class SectionTrigger : MonoBehaviour
+{
+    public GameObject roadSection;
+    public GameObject spawnPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Trigger"))
+        {
+            Instantiate(roadSection, new Vector3((float)-0.4, 4, (float)-188.8), Quaternion.identity);
+        }
+    }
+}
